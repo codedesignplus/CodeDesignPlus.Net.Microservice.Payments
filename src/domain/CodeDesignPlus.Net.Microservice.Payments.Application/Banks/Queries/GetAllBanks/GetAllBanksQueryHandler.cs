0c0ebbbe949fae52d89cd6e493a5020684739ba2
Wilzon Camilo Liscano Galindo <wliscano@codedesignplus.com>
2025-06-17 18:34:38 -0500

    feat: add host environment dependency to GetAllBanksQueryHandler for conditional data inclusion

--- src/domain/CodeDesignPlus.Net.Microservice.Payments.Application/Banks/Queries/GetAllBanks/GetAllBanksQueryHandler.cs
+++ src/domain/CodeDesignPlus.Net.Microservice.Payments.Application/Banks/Queries/GetAllBanks/GetAllBanksQueryHandler.cs
@@ -1,6 +1,8 @@
+using Microsoft.Extensions.Hosting;
+
 namespace CodeDesignPlus.Net.Microservice.Payments.Application.Banks.Queries.GetAllBanks;
 
-public class GetAllBanksQueryHandler(IBankRepository repository, IMapper mapper, ICacheManager cacheManager) : IRequestHandler<GetAllBanksQuery, List<BanksDto>>
+public class GetAllBanksQueryHandler(IBankRepository repository, IMapper mapper, ICacheManager cacheManager, IHostEnvironment hostEnvironment) : IRequestHandler<GetAllBanksQuery, List<BanksDto>>
 {
     private const string CACHE_KEY = "BanksList";
 
@@ -16,6 +18,16 @@ public class GetAllBanksQueryHandler(IBankRepository repository, IMapper mapper,
         var banks = await repository.GetAllAsync(cancellationToken);
 
         var data = mapper.Map<List<BanksDto>>(banks);
+
+        if(!hostEnvironment.IsProduction())
+            data.Add(new BanksDto
+            {
+                Id = Guid.Parse("442cdb72-3acd-4cf0-9264-a505cd139d41"),
+                Name = "FINANCIAL_INSTITUTION_CODE",
+                Description = "This is a test bank for development purposes.",
+                Code = "1022",
+                IsActive = true
+            });
 
         await cacheManager.SetAsync(CACHE_KEY, data, TimeSpan.FromHours(6));
 
